f2db10f5b79f0e91b3b4ba0f231515559b0d5a6e
unknown <daffatabianoo@gmail.com>
2024-11-16 19:17:56 +0700

    add(dto): data transfer object added

---
+++ src/auth/jwt-auth.guard.ts
@@ -0,0 +1,30 @@
+import { ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { JwtService } from "@nestjs/jwt";
+import {AuthGuard} from "@nestjs/passport";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+    constructor(
+        private reflector: Reflector, private jwtService: JwtService 
+    ){super();}
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+        if (isPublic) {
+            return true;
+        }
+        return super.canActivate(context);
+    }
+    handleRequest<TUser = any>(err: any, user: any, info: any, context: ExecutionContext, status?: any): TUser {
+        if (err || !user) {
+            throw err || new UnauthorizedException();
+        }
+        return user;
+    }
+
+}
